d5d2929f9f32ab830e142aa4428db98b97aa7847
Ivo Machado <ivo.machado@outlook.com.br>
2017-08-05 20:39:55 -0400

    Leitura do arquivo quase funcional

--- include/article.hpp
+++ include/article.hpp
@@ -1,7 +1,6 @@
 #ifndef ARTICLE_HPP
 #define ARTICLE_HPP
 
-#include <ctime>
 #include <string>
 
 /** 
@@ -9,13 +8,24 @@
  * \author Ivo Machado
  */
 class Article {
+public:
     int m_id;
     std::string m_title;
     int m_year;
     std::string m_authors;
-    int m_quotesQtde;
-    tm updateTime;
-    std::string snippet;
+    int m_quotes;
+    std::string m_updateTime;
+    std::string m_snippet;
+    /**
+     * Recebe uma linha de um CSV para criar um artigo com as informações 
+     * contidas nesta linha
+     */
+    Article(const char* csvEntry);
+
+    /**
+     * Construtor padrão
+     */
+    Article();
 };
 
 #endif
--- src/article.cpp
+++ src/article.cpp
@@ -1,1 +1,36 @@
 #include "article.hpp"
+#include <iostream>
+
+Article::Article(const char* csvEntry)
+{
+    char title[301], authors[1025], updateTime[101], snippet[1025];
+    int id, year, quotes;
+
+    std::string intPattern = "\"%d\";";
+    std::string stringPattern = "\"%[^;\"]\";";
+
+    std::string bigPattern = intPattern
+        + stringPattern
+        + intPattern
+        + stringPattern
+        + intPattern
+        + stringPattern
+        + stringPattern;
+
+    sscanf(csvEntry, bigPattern.c_str(),
+        &id,
+        title,
+        &year,
+        authors,
+        &quotes,
+        updateTime,
+        snippet);
+
+    m_id = id;
+    m_title = std::string(title);
+    m_year = year;
+    m_authors = std::string(authors);
+    m_quotes = quotes;
+    m_updateTime = std::string(updateTime);
+    m_snippet = std::string(snippet);
+}
