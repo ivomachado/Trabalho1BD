746c702ebf40ecbfc9fc47d7fe2ac9e2054b8247
Ivo Machado <ivo.machado@outlook.com.br>
2017-08-14 21:58:54 -0400

    Renomeação de funções

--- include/datatype.hpp
+++ include/datatype.hpp
@@ -7,16 +7,16 @@ namespace databaseTypes {
 
 class DataType {
 public:
-    short virtual writeBuffer(char* buffer, short begin) = 0;
-    short virtual readBuffer(char* buffer, short begin) = 0;
+    short virtual writeToBuffer(char* buffer, short begin) = 0;
+    short virtual readFromBuffer(char* buffer, short begin) = 0;
 };
 
 class Integer : public DataType {
     
 public:
     int m_data;
-    short writeBuffer(char* buffer, short begin);
-    short readBuffer(char* buffer, short begin);
+    short writeToBuffer(char* buffer, short begin);
+    short readFromBuffer(char* buffer, short begin);
     Integer(int value);
     Integer();
 };
@@ -25,8 +25,8 @@ class String : public DataType {
 public:
     short m_maxSize;
     char* m_data;
-    short readBuffer(char* buffer, short begin);
-    short writeBuffer(char* buffer, short begin);
+    short readFromBuffer(char* buffer, short begin);
+    short writeToBuffer(char* buffer, short begin);
     String(const char* data, short maxSize);
     String(short maxSize);
     ~String();
--- src/databaseTypes/datatype.cpp
+++ src/databaseTypes/datatype.cpp
@@ -23,25 +23,25 @@ databaseTypes::String::String(short maxSize)
     m_data = new char[maxSize];
 }
 
-short databaseTypes::String::writeBuffer(char* buffer, short begin)
+short databaseTypes::String::writeToBuffer(char* buffer, short begin)
 {
     strcpy(buffer + begin, m_data);
     return strlen(m_data) + begin;
 }
 
-short databaseTypes::Integer::writeBuffer(char* buffer, short begin)
+short databaseTypes::Integer::writeToBuffer(char* buffer, short begin)
 {
     sprintf(buffer + begin, "%d", m_data);
     return begin + sizeof(int);
 }
 
-short databaseTypes::String::readBuffer(char* buffer, short begin)
+short databaseTypes::String::readFromBuffer(char* buffer, short begin)
 {
     strcpy(m_data, buffer + begin);
     return strlen(m_data) + begin;
 }
 
-short databaseTypes::Integer::readBuffer(char* buffer, short begin)
+short databaseTypes::Integer::readFromBuffer(char* buffer, short begin)
 {
     sscanf(buffer + begin, "%d", &m_data);
     return begin + sizeof(int);
