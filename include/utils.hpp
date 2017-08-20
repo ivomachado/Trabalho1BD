e6469e7aa10e53c4e888364d3a9b2988f624dd54
Ivo Machado <ivo.machado@outlook.com.br>
2017-08-20 14:49:13 -0400

    Correção de passagem de parâmetros que eram feitos por valor para referência

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,10 +1,12 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <vector>
+
 namespace Utils {
 
 template <typename T>
-short writeVectorToBuffer(char* buffer, std::vector<T> m_data, short begin)
+short writeVectorToBuffer(char* buffer, std::vector<T> &m_data, short begin)
 {
     for (auto field : m_data) {
         begin = field.writeToBuffer(buffer, begin);
@@ -13,9 +15,9 @@ short writeVectorToBuffer(char* buffer, std::vector<T> m_data, short begin)
 }
 
 template <typename T>
-short readVectorFromBuffer(char* buffer, std::vector<T> m_data, short begin)
+short readVectorFromBuffer(char* buffer, std::vector<T> &m_data, short begin)
 {
-    for (auto field : m_data) {
+    for (auto& field : m_data) {
         begin = field.readFromBuffer(buffer, begin);
     }
     return begin;
