af3270f9dcdd287c309c45d3d7c8e1e2a4095864
Ivo Machado <ivo.machado@outlook.com.br>
2017-08-19 20:53:47 -0400

    Função com template para não repetir coisas

---
+++ include/utils.hpp
@@ -0,0 +1,26 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+namespace Utils {
+
+template <typename T>
+short writeVectorToBuffer(char* buffer, std::vector<T> m_data, short begin)
+{
+    for (auto field : m_data) {
+        begin = field.writeToBuffer(buffer, begin);
+    }
+    return begin;
+}
+
+template <typename T>
+short readVectorFromBuffer(char* buffer, std::vector<T> m_data, short begin)
+{
+    for (auto field : m_data) {
+        begin = field.readFromBuffer(buffer, begin);
+    }
+    return begin;
+}
+
+}
+
+#endif // !UTILS_HPP
