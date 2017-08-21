fca365fb5c36ed36ac9a07f213755c80a7e16cd0
Ivo Machado <ivo.machado@outlook.com.br>
2017-08-21 12:44:56 -0400

    Testes atualizados com int32_t

--- tests/tests-utils.cpp
+++ tests/tests-utils.cpp
@@ -1,16 +1,17 @@
 #include "catch.hpp"
 #include "utils.hpp"
 #include <limits>
+#include <cstdint>
 
 TEST_CASE("Escrita de inteiro em buffer", "[.][intConvetion]")
 {
-    int x = 0;
+    int32_t x = 0;
     char buffer[8];
-    for (x = 0; x <= std::numeric_limits<int>::max()/1024; x++) {
+    for (x = 0; x <= std::numeric_limits<int32_t>::max()/1024; x++) {
         Utils::intToCharArray(x, buffer + 4);
         REQUIRE(x == Utils::charArrayToInt(buffer + 4));
     }
-    for (x = 0; x >= std::numeric_limits<int>::min()/1024; x--) {
+    for (x = 0; x >= std::numeric_limits<int32_t>::min()/1024; x--) {
         Utils::intToCharArray(x, buffer + 4);
         REQUIRE(x == Utils::charArrayToInt(buffer + 4));
     }
