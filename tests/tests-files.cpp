ed0399f5fcfc3dc399c95bf668592be818d9d2c9
Ivo Machado <ivo.machado@outlook.com.br>
2017-08-23 14:33:02 -0400

    Testes de inserção no arquivo hash

--- tests/tests-files.cpp
+++ tests/tests-files.cpp
@@ -7,9 +7,11 @@
 #include <limits>
 #include <vector>
 
+using namespace std;
+
 TEST_CASE("Escrita de Header de HashFile")
 {
-    std::string firstBitMap;
+    string firstBitMap;
     {
         HashFile hashfile = HashFile::Create("hash.bin");
         hashfile.m_blocksMap.set(3, true);
@@ -24,4 +26,35 @@ TEST_CASE("Escrita de Header de HashFile")
         REQUIRE(hashfile.m_overflowBlocks == 3);
     }
     remove("hash.bin");
+}
+
+TEST_CASE("Escrita de registro no HashFile")
+{
+    vector<Field> recordFields;
+    recordFields.emplace_back(Field::asInteger());
+    recordFields.emplace_back(Field::asString(DiskBlock::AVAILABLE_SIZE - 4));
+    int32_t aIndex, bIndex, cIndex;
+
+    Record a(recordFields), b(recordFields), c(recordFields);
+
+    a.m_data[0].m_integer = 2;
+    a.m_data[1].m_string = string("bankai");
+
+    b.m_data[0].m_integer = 2;
+    b.m_data[1].m_string = string("shikai");
+
+    c.m_data[0].m_integer = 2;
+    c.m_data[1].m_string = string("cero");
+
+    string firstBitMap;
+    {
+        HashFile hashfile = HashFile::Create("hash.bin");
+        aIndex = hashfile.insert(a);
+        bIndex = hashfile.insert(b);
+        cIndex = hashfile.insert(c);
+        REQUIRE(hashfile.m_overflowBlocks == 2);
+        REQUIRE(aIndex == 2);
+        REQUIRE(bIndex == HashFile::NUMBER_BLOCKS + 0);
+        REQUIRE(cIndex == HashFile::NUMBER_BLOCKS + 1);
+    }
 }
