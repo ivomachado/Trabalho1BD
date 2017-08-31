83cf9bb47ccb4fd528192564a0cc9c69b4226fef
Ivo Machado <ivo.machado@outlook.com.br>
2017-08-31 09:06:48 -0400

    Programa para debug da árvore

--- src/program.cpp
+++ src/program.cpp
@@ -2,6 +2,7 @@
 #include "article.hpp"
 #include "field.hpp"
 #include "hashfile.hpp"
+#include "indexfile.hpp"
 #include "record.hpp"
 #include <cstdio>
 #include <cstdlib>
@@ -27,27 +28,35 @@ void Program::upload()
 
 int main()
 {
-    vector<Field> recordFields;
-    recordFields.emplace_back(Field::asInteger());
-    recordFields.emplace_back(Field::asString(DiskBlock::AVAILABLE_SIZE - 4));
-    int32_t aIndex, bIndex, cIndex;
-
-    Record a(recordFields), b(recordFields), c(recordFields);
-
-    a.m_data[0].m_integer = 2;
-    a.m_data[1].m_string = string("bankai");
-
-    b.m_data[0].m_integer = 2;
-    b.m_data[1].m_string = string("shikai");
-
-    c.m_data[0].m_integer = 2;
-    c.m_data[1].m_string = string("cero");
-
-    string firstBitMap;
+    Field field1 = Field::asString("1", 1024);
+    Field field2 = Field::asString("2", 1024);
+    Field field3 = Field::asString("3", 1024);
+    Field field4 = Field::asString("4", 1024);
+    Field field5 = Field::asString("5", 1024);
+    Field field6 = Field::asString("6", 1024);
+    Field field7 = Field::asString("7", 1024);
+    Field field8 = Field::asString("8", 1024);
     {
-        HashFile hashfile = HashFile::Create("hash.bin");
-        aIndex = hashfile.insert(a);
-        bIndex = hashfile.insert(b);
-        cIndex = hashfile.insert(c);
+        IndexFile indexFile = IndexFile::Create("index.bin");
+        indexFile.insert(field1, 1);
+        indexFile.insert(field2, 2);
+        indexFile.insert(field3, 3);
+        indexFile.insert(field4, 4);
+        indexFile.insert(field5, 5);
+        indexFile.insert(field6, 6);
+        indexFile.insert(field7, 7);
+        indexFile.insert(field8, 8);
+    }
+
+    {
+        IndexFile indexFile = IndexFile::Open("index.bin");
+        indexFile.search(field1);
+        indexFile.search(field2);
+        indexFile.search(field3);
+        indexFile.search(field4);
+        indexFile.search(field5);
+        indexFile.search(field6);
+        indexFile.search(field7);
+        indexFile.search(field8);
     }
 }
