802a721959a26e00d3fe7e04ba0d59b9ccaff93a
Ivo Machado <ivo.machado@outlook.com.br>
2017-09-03 00:09:31 -0400

    teste da nova versão do bitmap

--- tests/tests-utils.cpp
+++ tests/tests-utils.cpp
@@ -22,9 +22,9 @@ TEST_CASE("Escrita de inteiro em buffer", "[.][intConvetion]")
 
 TEST_CASE("Operações com bitmap")
 {
-    const int testSize = 31;
-    Utils::BitMap m(testSize + 1), mTestSize(testSize);
-    REQUIRE(mTestSize.m_data.size() == 4);
+    const int testSize = 64;
+    Utils::BitMap m(testSize), mTestSize(testSize - 1);
+    REQUIRE(mTestSize.m_data.size() == 8);
     for (int i = 0; i < testSize; i++) {
         REQUIRE(!m.get(i));
     }
@@ -37,13 +37,17 @@ TEST_CASE("Operações com bitmap")
         REQUIRE(m.get(i) == i % 2);
     }
     std::vector<Field> fields{
-        Field::asInteger(), Field::asByteArray(1), Field::asByteArray(1), Field::asByteArray(2), Field::asInteger(),
+        Field::asInteger(), Field::asByteArray(1), Field::asByteArray(1), Field::asInteger(),
     };
     // m.m_data = "data";
     Record rec(fields);
-    std::vector<Record> recs{ rec };
-    m.write(recs);
-    Utils::BitMap m2(recs);
+    std::vector<DiskBlock> blocks;
+    for (int i = 0; i < 4; i++) {
+        blocks.emplace_back(fields);
+        blocks.back().insert(rec);
+    }
+    m.write(blocks);
+    Utils::BitMap m2(blocks);
 
     REQUIRE(m.m_data == m2.m_data);
 
